5293a2413b2c41c8cb7b2874c84e6136bf7c5b33
Luis Enrique Suárez <luissuarezdev@gmail.com>
2018-02-12 12:09:46 -0400

    serve

--- serve.go
+++ serve.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/luewell/framework/bootstrap"
+import (
+	"github.com/luewell/framework/bootstrap"
+)
+
+func init() {
+	bootstrap.App()
+}
 
 func main() {
-	bootstrap.App()
+
 }
