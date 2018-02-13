29edb563bf10b3f8d36247e832f8e94d17137fae
Luis Enrique Suárez <luissuarezdev@gmail.com>
2018-02-13 10:27:13 -0400

    url

--- routes/web.go
+++ routes/web.go
@@ -1,11 +1,17 @@
 package routes
 
 import (
+	"fmt"
+
 	"github.com/luewell/framework/Illuminate/Http"
 )
 
 func Web(http *Http.Router) {
-	/*http.Get("/", func(ctx *Http.Context) {
+	http.Get("/", func(ctx *Http.Context) {
+		fmt.Fprintln(ctx.ResponseWriter, "It Works!")
+	})
 
-	})*/
+	http.Get("/hello/world", func(ctx *Http.Context) {
+		fmt.Fprintln(ctx.ResponseWriter, "Hello world!")
+	})
 }
